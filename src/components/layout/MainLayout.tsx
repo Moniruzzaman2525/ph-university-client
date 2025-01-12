0c1f1489d6a204d2601a3a4ed1c7b50ef6350e3e
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-01-12 12:55:10 +0600

    module -3 done

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,54 +1,72 @@
 
-import React, { createElement } from 'react';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import React from 'react';
+import { Layout, Menu, MenuProps } from 'antd';
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-  (icon, index) => ({
-    key: String(index + 1),
-    icon: createElement(icon),
-    label: `nav ${index + 1}`,
-  }),
-);
+const items: MenuProps['items'] = [
+    {
+        key: "111",
+        label: 'Dashboard'
+    },
+    {
+        key: "10111",
+        label: 'Profile'
+    },
+    {
+        key: "0111",
+        label: 'User Management',
+        children: [
+            {
+                key: "1213211",
+                label: 'Create Admin'
+            },
+            {
+                key: "10111552",
+                label: 'Create Student'
+            },
+        ]
+    },
+]
 
 const MainLayout: React.FC = () => {
 
 
-  return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0}} />
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360
-            }}
-          >
-            <h1>GThe main content should be there</h1>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
-      </Layout>
-    </Layout>
-  );
+    return (
+        <Layout style={{ height: '100vh' }}>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+            >
+                <div style={{color: 'white', textAlign: 'center', height: '4rem', display: 'flex', justifyContent: 'center', justifyItems: 'center'}}>
+                    <h1 style={{height: '100%'}}>Ph Uni</h1>
+                </div>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0 }} />
+                <Content style={{ margin: '24px 16px 0' }}>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360
+                        }}
+                    >
+                        <h1>The main content should be there</h1>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
 };
 
 export default MainLayout;
