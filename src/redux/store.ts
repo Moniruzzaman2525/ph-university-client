390cc236bd8ad00359d4388f61d73e286e5550de
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-01-13 17:02:53 +0600

    auth slice start

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from './feathers/auth/authSlice'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authReducer
+    },
 })
 
 
