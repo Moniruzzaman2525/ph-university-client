7c527ccd572102b1b6e7e2174f517bf24591f178
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-01-13 18:05:33 +0600

    login api end point done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,31 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form"
+import { useLoginMutation } from "../redux/feathers/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/feathers/auth/authSlice";
+
 export const Login = () => {
+  const { register, handleSubmit } = useForm()
+  const [login, { error }] = useLoginMutation()
+
+  const dispatch = useAppDispatch()
+
+  const onSubmit = async (data) => {
+    const res = await login(data).unwrap()
+    dispatch(setUser({
+      user: {},
+      token: res.data.accessToken
+    }))
+  }
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="id">Id: </label>
+        <input type="text" id="id" {...register('id')} />
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register('password')} />
+        <Button htmlType="submit">Login</Button>
+      </form>
+    </div>
   )
 }
